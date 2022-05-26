622c81c01f07fde1bc917d1e5009a64a984ca906
badcast <lmecomposer@gmail.com>
2022-05-27 02:15:56 +0600

    Upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#badcsat for cast!
+#enjoy <^_^>
+
+echo 'installing Visual Studio Dark X themes for QT-Creator'
+f=~/.config/QtProject/qtcreator/styles/
+mkdir -p $f 
+
+#copy to 
+cp ./VisualStudioDarkX.xml $f
+
+echo 'installed'
+
+	
